59f836e74b5c1b6551ff229da4e99c8c700cac83
ChinaWxq <1970014719@qq.com>
2020-02-16 12:41:09 +0800

    20th project

---
+++ Project 20 - 3DTouchQuickAction/Project 20 - 3DTouchQuickAction/DataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DataViewController.swift
+//  Project 20 - 3DTouchQuickAction
+//
+//  Created by Ryan on 2020/2/16.
+//  Copyright © 2020 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
